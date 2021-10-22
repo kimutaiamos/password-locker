c04d26cb34ddf6d7b8752a36fc681a2366594bf3
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-10-22 21:04:17 +0300

    added a function that generates password automatically

--- password_locker.py
+++ password_locker.py
@@ -1,14 +1,39 @@
 import pyperclip
-from user_credentials import User, credential
-def create_user(firstname, lastname, password)
-"""
-function to create a new user account
-"""
-new_user = User(firstname,lastname,password)
+from user_credentials import User, Credential
+
+def create_user(fname,lname,password):
+	'''
+	Function to create a new user account
+	'''
+	new_user = User(fname,lname,password)
 	return new_user
-   
-    def save_user(user):
+
+def save_user(user):
 	'''
 	Function to save a new user account
 	'''
 	User.save_user(user)
+
+
+def verify_user(first_name,password):
+	'''
+	Function that verifies the existance of the user before creating credentials
+	'''
+	checking_user = Credential.check_user(first_name,password)
+	return checking_user
+
+
+def generate_password():
+	'''
+	Function to generate a password automatically
+	'''
+	gen_pass = Credential.generate_password()
+	return gen_pass
+
+def create_credential(user_name,site_name,account_name,password):
+	'''
+	Function to create a new credential
+	'''
+	new_credential=Credential(user_name,site_name,account_name,password)
+	return new_credential
+
