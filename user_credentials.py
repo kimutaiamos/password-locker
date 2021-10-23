306a11867ae253763243f77a8e6ed7b8d7f3816b
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-10-23 12:14:38 +0300

    created a method for password generator

--- user_credentials.py
+++ user_credentials.py
@@ -22,3 +22,26 @@ class User:
 		self.first_name = first_name
 		self.last_name = last_name
 		self.password = password
+def save_user(self):
+    """
+    function to save a newly created user
+    """
+    User.users_list.append(self)
+class credential:
+    """
+    class to create credentials ,generate passwords and save their information
+    """
+#class variables
+credential_list = []
+user_credentials_list = []
+@classmethod
+def check_user(cls,first_name,password):
+    """
+    method that checks if the name and password entered matches with the original enries
+    """
+    current_user = ''
+    for user in User.users_list:
+        if(user.first_name == first_name and user.password == password):
+            current_user = user.first_name
+            return current_user
+            
